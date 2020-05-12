269d1f4f2acc57b6af14d9e4412baca6dd1e58ba
Saurabh <saurabhbharti9@gmail.com>
2020-05-13 00:58:04 +0530

    added form to signup compoenent

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -2,10 +2,32 @@ import React from "react";
 
 function SignUp() {
   return (
-    <div>
+    <div className="card col-6 mt-4">
       <h1>
-        <span className="badge badge-dark">SignUp</span>
+        <span className="badge badge-dark mt-2">Few Details Please!</span>
       </h1>
+
+      <form>
+        <input
+          type="text"
+          name="username"
+          placeholder="select a username"
+          className="form-control"
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="password"
+          className="form-control mt-2"
+        />
+        <input
+          type="password"
+          name="confirmPwd"
+          placeholder="confirm password"
+          className="form-control mt-2"
+        />
+        <button className="form-control mt-2 btn btn-success">SignUp</button>
+      </form>
     </div>
   );
 }
